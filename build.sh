7b4b6b2d0b2a15889aa3ac12474042db7f91ef94
wsxqaz789456 <sindaea77@gmail.com>
2023-02-17 11:14:29 +0900

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 poetry install
-
+pip install --upgrade pippip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
