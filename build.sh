cfdcdf3f1b88c338a071455feb35591991a415d3
wsxqaz789456 <sindaea77@gmail.com>
2023-02-17 11:54:40 +0900

    build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install --upgrade pip
 
 poetry install
-pip install --force-reinstall -U setuptools
+pip install --upgrade pippip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
